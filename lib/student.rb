f8f87b281bceee1bcd7cbbc1dabcc76581a2f4c9
Dani Lyn <dlyn1110@gmail.com>
2018-07-05 21:16:04 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -21,8 +21,4 @@ class Student < InteractiveRecord
     column_names.compact
   end
 
-   def initialize(options={})
-     options.each do |property, value|
-       self.send("#{property}=", value)
-   end
-  end
+end
