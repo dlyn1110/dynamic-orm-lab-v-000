da2d28860e002d1e065f9d2ccb07a0f5714e8678
Dani Lyn <dlyn1110@gmail.com>
2018-07-05 21:34:07 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -32,17 +32,17 @@ class  InteractiveRecord
    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
- def table_name_for_insert
+  def table_name_for_insert
    self.class.table_name
- end
+  end
 
- def values_for_insert
+  def values_for_insert
    values = []
    self.class.column_names.each do |col_name|
      values << "'#{send(col_name)}'" unless send(col_name).nil?
    end
    values.join(", ")
- end
+  end
 
  def col_names_for_insert
    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
@@ -53,8 +53,7 @@ class  InteractiveRecord
    DB[:conn].execute(sql)
  end
 
-end
-
+ end
 
 
 
