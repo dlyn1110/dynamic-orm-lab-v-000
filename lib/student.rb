1a25abaa2c39e09d7caabbfb8ddc5d0621f7fcde
Dani Lyn <dlyn1110@gmail.com>
2018-07-05 21:03:04 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,17 +4,8 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
-  def self.column_names
-  DB[:conn].results_as_hash = true
-
-  sql = "pragma table_info('#{table_name}')"
-
-  table_info = DB[:conn].execute(sql)
-  column_names = []
-  table_info.each do |row|
-    column_names << row["name"]
-  end
-  column_names.compact
-end
+   def self.table_name
+     self.to_s.downcase.pluralize
+   end
 
 end
