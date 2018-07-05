25171024d39316f8f3b48fb92da7cbf6b1352db9
Dani Lyn <dlyn1110@gmail.com>
2018-07-05 21:27:01 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,9 +1,24 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
-class Song < InteractiveRecord
-  self.column_names.each do |col_name|
-  attr_accessor col_name.to_sym
-end
+class  InteractiveRecord
 
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    DB[:conn].results_as_hash = true
+
+    sql = "pragma table_info('#{table_name}')"
+
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+    table_info.each do |row|
+      column_names << row["name"]
+    end
+    column_names.compact
+  end
+
+  
 end
